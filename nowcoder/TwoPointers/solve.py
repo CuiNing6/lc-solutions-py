ed56e7ce5bf096a0afe368ba17da17033a4debc8
cuining8 <cuining8@jd.com>
2022-08-29 18:09:35 +0800

    add solve

---
+++ nowcoder/TwoPointers/solve.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+'''
+描述
+写出一个程序，接受一个字符串，然后输出该字符串反转后的字符串。（字符串长度不超过1000）
+
+数据范围： 0≤n≤1000
+要求：空间复杂度 O(n)，时间复杂度 O(n)
+
+示例1
+输入："abcd"
+返回值："dcba"
+
+示例2
+输入：""
+返回值：""
+'''
+#
+# 反转字符串
+# @param str string字符串
+# @return string字符串
+#
+class Solution:
+    def solve(self , str ):
+        # write code here
+        pass
