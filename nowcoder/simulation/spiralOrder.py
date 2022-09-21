eb65cc1f7b0d3d1208b104ffe5bb9c100bfcec68
CuiNing <946105802@qq.com>
2022-09-21 22:38:02 +0800

    add simulation

---
+++ nowcoder/simulation/spiralOrder.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+'''
+描述
+给定一个m x n大小的矩阵（m行，n列），按螺旋的顺序返回矩阵中的所有元素。
+
+数据范围：0≤n,m≤10，矩阵中任意元素都满足 ∣val∣≤100
+要求：空间复杂度 O(nm)，时间复杂度 O(nm)
+
+
+示例1
+输入：[[1,2,3],[4,5,6],[7,8,9]]
+返回值：[1,2,3,6,9,8,7,4,5]
+
+示例2
+输入：[]
+返回值：[]
+'''
+
+#
+#
+# @param matrix int整型二维数组
+# @return int整型一维数组
+#
+class Solution:
+    def spiralOrder(self , matrix ):
+        # write code here
+        pass
